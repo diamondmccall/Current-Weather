8726859da0c3376174ebf45078c950684f336f17
Diamond Mccall <singlikediamond@gmail.com>
2023-03-12 20:05:11 -0600

    inital commit

---
+++ WeatherAPI/WeatherAPI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using WeatherAPI;
+
+namespace WeatherAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = File.ReadAllText("appsettings.json");
+            string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+
+            Console.WriteLine("What is your zipcode.");
+            var zipCode = Console.ReadLine();
+
+            var apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&appid={APIKey}&units=imperial";
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Currently it is {WeatherMap.Temp(apiCall)} degrees F in your location.");
+
+
+        }
+    }
+}
---
+++ WeatherAPI/WeatherAPI/WeatherMap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+
+
+namespace WeatherAPI
+{
+    public class WeatherMap
+    {
+        public static double Temp(string apiCall)
+        {
+            var client = new HttpClient();
+            var response = client.GetStringAsync(apiCall).Result;
+            var temp = double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+            return temp;
+        }
+    }
+}
+
